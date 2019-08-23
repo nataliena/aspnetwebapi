8c249dc2c856be431cb29266cbe28f95f7974c60
nataliena <47991729+nataliena@users.noreply.github.com>
2019-08-23 23:27:09 +0200

    Add files via upload

---
+++ FrontEnd post method - add users/WebApplication1/Services/TaskRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Services
+{
+    public class TaskRepository
+    {
+        private readonly ToDoDbContext db;
+        public TaskRepository (ToDoDbContext context)
+        {
+            db = context;
+        }
+        public List<Task> GetAll()
+        {
+            return db.Tasks.ToList();
+        }
+    }
+}
