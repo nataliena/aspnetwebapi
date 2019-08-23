2ef7ca9b7086cdc6028a224fc3f5079506c29556
nataliena <47991729+nataliena@users.noreply.github.com>
2019-08-23 23:25:52 +0200

    Add files via upload

---
+++ FrontEnd post method - add users/WebApplication1/Controllers/TasksController.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections.Generic;
+using Task = WebApplication1.Models.Task;
+
+
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using WebApplication1.Services;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TasksController : ControllerBase
+    {
+        private static IList<Task>_tasks = new List<Task>
+        {
+            new Task
+            {
+              Id=1,
+              Title="Task 1",
+              Description="Description of Task 1"
+              },
+            new Task
+            {
+              Id=2,
+              Title="Task 2",
+              Description="Description of Task 2"
+              },
+            new Task
+            {
+              Id=3,
+              Title="Task 3",
+              Description="Description of Task 3"
+              },
+            new Task
+            {
+              Id=4,
+              Title="Task 4",
+              Description="Description of Task 4"
+              }
+
+        };
+
+
+        private readonly TaskRepository repo;
+
+        public TasksController (TaskRepository _repo)
+        {
+            repo = _repo;
+
+        }
+
+        // GET api/tasks
+      
+        [HttpGet]
+        //public ActionResult<string> Get()
+        public ActionResult<IList<Task>> Get()
+        {
+            //return StatusCode(StatusCodes.Status404NotFound, "Hello World");
+            //return NotFound("Hello World");
+            //return Ok("Hello World");
+
+            return Ok(_tasks);
+
+        }
+        [HttpGet("{id}")]
+        public ActionResult<Task> Get(int id)
+        {
+            Task task = _tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task == null)
+            {
+                return NotFound($"Task with id : {id} not found");
+                
+            }
+            return Ok(task);
+
+        }
+        // api/tasks
+        [HttpPost]
+        public ActionResult<string> Post([FromBody]Task obj)
+        {
+            Task newTask = new Task
+            {
+                Id = _tasks.Count() + 1,
+                Title = obj.Title,
+                Description = obj.Description
+
+            };
+            _tasks.Add(newTask);
+            return Ok("Get a post request");
+        }
+
+        [HttpPut("{id}")]
+        public ActionResult<Task> Put([FromBody]Task obj, int id)
+        {
+            Task task = _tasks.FirstOrDefault(t => t.Id == id);
+
+            if (task == null)
+            {
+                return NotFound($"Task with id : {id} not found");
+
+            }
+
+            task.Title = obj.Title;
+            task.Description = obj.Description;
+
+
+            return Ok("Get a put request");
+        }
+
+
+
+
+
+        //// GET api/values/5
+        //[HttpGet("{id}")]
+        //public ActionResult<string> Get(int id,[FromQuery]string user)
+        //{
+        //    return $"endpoint {id}, from query {user}";
+        //}
+    }
+}
---
+++ FrontEnd post method - add users/WebApplication1/Controllers/UsersController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+
+    }
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private static IList<User> _users = new List<User>
+        {
+            new User
+            {
+              Id=1,
+              FirstName="User 1 FirstName",
+              Lastname="User 1 LastName",
+              Age=30
+              },
+            new User
+            {
+               Id=2,
+              FirstName="User 2 FirstName",
+              Lastname="User 2 LastName",
+              Age=35
+              },
+            new User
+            {
+                Id=3,
+              FirstName="User 3 FirstName",
+              Lastname="User 3 LastName",
+              Age=25
+              },
+            new User
+            {
+                Id=4,
+              FirstName="User 4 FirstName",
+              Lastname="User 4 LastName",
+              Age=28
+              },
+
+            new User
+            {
+                Id=5,
+              FirstName="User 5 FirstName",
+              Lastname="User 5 LastName",
+            
+              }
+
+        };
+        [HttpGet]
+        public ActionResult <List<User>> GetAllUsers()
+        {
+            return Ok(_users);
+
+
+        }
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUserByIndex(int id)
+        {
+            User user = _users.FirstOrDefault(u => u.Id == id);
+
+            if (user == null)
+            {
+                return NotFound($"User with id : {id} not found");
+
+            }
+            return Ok(user);
+
+        }
+        [HttpGet("{id}/isadult")]
+        public ActionResult<User> GetAge(int id)
+        {
+            User user = _users.FirstOrDefault(u => u.Id == id);
+            if (user == null)
+            {
+                return NotFound($"User with id : {id} not found");
+            }
+            else if (user.Age >= 18)
+            {
+                return Ok($"User with id :{id} is adult");
+            }
+            else
+            {
+                return Ok($"User with id :{id} is'nt adult");
+            }
+
+
+
+        }
+
+        [HttpPost]
+        public ActionResult<string> Post([FromBody]User obj)
+        {
+            User newUser = new User
+            {
+                Id = _users.Count() + 1,
+                FirstName = obj.FirstName,
+                Lastname = obj.Lastname
+                
+            };
+            _users.Add(newUser);
+            return Ok("Get a post request");
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
